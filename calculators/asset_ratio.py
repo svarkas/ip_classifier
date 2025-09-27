c463d473968b910fc584fcb9c187d49a01adbeab
svarkas <stvarkas@gmail.com>
2025-09-27 18:28:10 +0300

    start asset ratio

---
+++ calculators/asset_ratio.py
@@ -0,0 +1,6 @@
+from .base import BaseCalculator
+
+class AssetRatio(BaseCalculator):
+    def calculate(self, records: list[dict] ) -> list[dict]:
+        from or record in records:
+            print(record["endpoint"])
