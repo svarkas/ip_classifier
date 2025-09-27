a59c4f93a2f15b762a03c4a0153846d989899892
svarkas <stvarkas@gmail.com>
2025-09-27 18:39:59 +0300

    start asset ratio

--- calculators/asset_ratio.py
+++ calculators/asset_ratio.py
@@ -2,5 +2,5 @@ from .base import BaseCalculator
 
 class AssetRatio(BaseCalculator):
     def calculate(self, records: list[dict] ) -> list[dict]:
-        from or record in records:
+        for record in records:
             print(record["endpoint"])
--- ip_classifier.py
+++ ip_classifier.py
@@ -3,14 +3,15 @@ import os
 from libs.import_data import Importer
 from calculators.doi import Doi
 from calculators.asset_ratio import AssetRatio
+from copy import deepcopy
 
 def main(argv):
     importer = Importer()
-    records = importer.read_log(argv[1], 1000)
+    records = importer.read_log(argv[1], 2000)
     dc = Doi()
-    dc.calculate(records)
+    dc.calculate(deepcopy(records))
     arc = AssetRatio()
-    arc.calculate(records)
+    arc.calculate(deepcopy(records))
 
 if __name__ == "__main__":
     main(sys.argv)
