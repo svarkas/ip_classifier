4f37d7eb17805c34980886d22737b888745e0f7a
svarkas <stvarkas@gmail.com>
2025-10-02 20:25:25 +0300

    calculating asset requests

--- calculators/asset_ratio.py
+++ calculators/asset_ratio.py
@@ -1,4 +1,5 @@
 from .base import BaseCalculator
+from collections import Counter
 
 class AssetRatio(BaseCalculator):
     def calculate(self, records: list[dict] ) -> list[dict]:
@@ -9,7 +10,7 @@ class AssetRatio(BaseCalculator):
                 ip_minute_key = f"{record["ip_address"]};{record["time"]}"
                 ip_minute_keys.append(ip_minute_key)
                 ip_minute_endpoint_end_key = f"{ip_minute_key};{record["endpoint"].split('.')[-1]}"
-                ip_minute_endpoint_end_keys.append(ip_minute_endpoint_end_keys)
+                ip_minute_endpoint_end_keys.append(ip_minute_endpoint_end_key)
         
         total_requests = []
         for key in ip_minute_keys:
@@ -17,6 +18,13 @@ class AssetRatio(BaseCalculator):
             count = ip_minute_keys.count(key)
             total_requests.append({"ip_address": ip_address, "total_requests": count})
         
-        asset_requests = 
-            for key in ip_minute_endpoint_end_keys:
+        ip_endpoints = [] 
+        assets_endings = ['css', 'js', 'png' ,'jpg' ,'jpeg' ,'gif' ,'ico' ,'woff' ,'woff2' ,'ttf']
+        for key in ip_minute_endpoint_end_keys:
             *_, endpoint_end = key.split(';')
+            if endpoint_end in assets_endings:
+                ip_address, _, endpoint = key.split(';')
+                count = ip_minute_endpoint_end_keys.count(key)
+                ip_endpoints.append({"ip_address": ip_address, "endpoint": endpoint})
+        asset_requests = Counter(asset_req_ip["ip_address"] for asset_req_ip in ip_endpoints)
+        print(asset_requests)
