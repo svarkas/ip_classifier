f49a836711781af3357fdeb980ab1a3b2470d193
svarkas <stvarkas@gmail.com>
2025-10-05 01:58:05 +0300

    join assets total

--- calculators/asset_ratio.py
+++ calculators/asset_ratio.py
@@ -4,7 +4,7 @@ from collections import Counter
 class AssetRatio(BaseCalculator):
 
     # TODO unique ip and sum of requests
-    def total_requests(self, records: list[dict]) -> list[dict]:
+    def total_requests(self, records: list[dict]) -> dict:
         ip_minute_keys = []
         for record in records:
             if record["endpoint"]:
@@ -17,13 +17,18 @@ class AssetRatio(BaseCalculator):
             count = ip_minute_keys.count(key)
             requests_per_key.append({"ip_address": ip_address, "total_requests": count})
         
-        sum_req_per_key = Counter()
+        total_requests = {}
         for rk in requests_per_key:
-            sum_req_per_key.update(rk)
+            ip_address = rk["ip_address"]
+            t_requests = rk["total_requests"]
+            if ip_address in total_requests:
+                total_requests[ip_address] += t_requests
+            else:
+                total_requests[ip_address] = t_requests
 
-        return sum_req_per_key
+        return total_requests 
 
-    def asset_requests(self, records: list[dict]) -> list[dict]:
+    def asset_requests(self, records: list[dict]) -> dict:
         ip_minute_endpoint_end_keys = []
         for record in records:
             if record["endpoint"]:
@@ -32,24 +37,25 @@ class AssetRatio(BaseCalculator):
         
         ip_endpoints = [] 
         assets_endings = ['css', 'js', 'png' ,'jpg' ,'jpeg' ,'gif' ,'ico' ,'woff' ,'woff2' ,'ttf']
-        asset_requests = []
         for key in ip_minute_endpoint_end_keys:
             *_, endpoint_end = key.split(';')
             if endpoint_end in assets_endings:
                 ip_address, _, endpoint = key.split(';')
-                count = ip_minute_endpoint_end_keys.count(key)
                 ip_endpoints.append({"ip_address": ip_address, "endpoint": endpoint})
-        asset_requests = Counter(asset_req_ip["ip_address"] for asset_req_ip in ip_endpoints)
+        
+        asset_requests = {}
+        for ie in ip_endpoints:
+            ip_address = ie["ip_address"]
+            if ip_address in asset_requests:
+                asset_requests[ip_address] += 1
+            else:
+                asset_requests[ip_address] = 1
         return asset_requests
 
-    def join_data_by_ip(self, total_requests: list[dict], asset_request: list[dict]) -> list[dict]:
-        total_requests_indexed = {tr["ip_address"]: tr for tr in total_requests}
-
-        for ar in asset_request:
-            if ar["ip_address"] in total_requests_indexed:
-                pass
+    def join_data_by_ip(self, total_requests: dict, asset_request: dict)-> list[tuple[str, int, int]]:
+        return [(ip, total_requests.get(ip), asset_request.get(ip)) for ip in total_requests | asset_request]
 
     def calculate(self, records: list[dict] ) -> list[dict]:
         total_requests = self.total_requests(records)
         asset_requests = self.asset_requests(records)
-        print(total_requests)
+        print(self.join_data_by_ip(total_requests, asset_requests))
