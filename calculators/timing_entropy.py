894ed0afd8ea5053750907938c2c8b4639f433ca
svarkas <stvarkas@gmail.com>
2026-02-21 00:35:18 +0200

    working on time entropy

--- calculators/timing_entropy.py
+++ calculators/timing_entropy.py
@@ -1,14 +1,16 @@
 from .base import BaseCalculator
 import pandas as pd
+import sys
 
 class TimingEntropy(BaseCalculator):
 
     def calculate(self, records: list[dict]) -> None:
         records_df = pd.DataFrame(records)  
         records_df["time"] = pd.to_datetime(records_df["time"], format='%d/%b/%Y:%H:%M:%S' )
+        records_df["unix_time"] = records_df["time"].astype('int64')
         records_df = records_df.sort_values(["ip_address", "time"])
         records_grouped = records_df.groupby("ip_address")
-
-        for ip_address, group in records_grouped:
-            print(f"IP:{ip_address}")
-            print(group, "\n")
+        ip_deltas = {}
+        for ip_address, ip_data in records_grouped:
+            ip_deltas[ip_address] = [ip_data["unix_time"][i] - ip_data["unix_time"][i-1] for i in range(1, len(ip_data), 1)]
+        print(ip_deltas)
