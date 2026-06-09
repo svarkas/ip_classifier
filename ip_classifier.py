2035efc40bbe41a047cbffd53b48f4d19474f2e2
svarkas <stvarkas@gmail.com>
2026-06-09 16:51:38 +0300

    random ip country

--- ip_classifier.py
+++ ip_classifier.py
@@ -7,6 +7,11 @@ from calculators.timing_entropy import TimingEntropy
 from copy import deepcopy
 import pandas as pd 
 from libs.rt_tree import RTtree
+from libs.ip_info import get_ip_country_code
+import json
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from typing import Optional
+from random import randrange
 
 def compine_features(doi_variance: dict, assets_ratio: dict, time_entropy: dict) -> pd.DataFrame:
     features_df = pd.DataFrame({
@@ -34,9 +39,46 @@ def predict(node, features):
     else:
         return predict(node.right, features)
 
+def process(ip: str) -> Optional[dict]:
+    country_code = get_ip_country_code(ip)
+    if country_code is None:
+        return None
+
+    return {
+        "ip": ip,
+        "country_code": country_code
+    }
+
+def geolocate_records_out(records:list[dict]):
+    with ThreadPoolExecutor(max_workers=8) as executor, open("data/ip_country.jsonl", "w", encoding="utf-8") as fw:
+
+        futures = [executor.submit(process, r["ip_address"]) for r in records]
+        
+        for future in as_completed(futures):
+            row = future.result()
+            if row is not None:
+                fw.write((json.dumps(row, ensure_ascii=False) + "\n"))
+
+def geolocate_records_df(records: list[dict]):
+    ip_df = pd.DataFrame({
+        "ip": [r["ip_address"] for r in records],
+        "country": [geolocate_ip(r["ip_address"]) for r in records]
+    })
+    print(ip_df)
+
+def sample(records: list[dict], count: int, max: int) -> list[dict]:
+    samples = []
+    for i in range(0, count, 1):
+        samples.append(records[randrange(len(records)-1)])
+    return samples
+
 def main(argv):
     importer = Importer()
-    records = importer.read_log(argv[1], 2000)
+    records = importer.read_log(argv[1], 1000)
+    records = sample(deepcopy(records), 100, 1000)
+    geolocate_records_out(records)
+    exit()
+
     dc = Doi()
     doi_variance = dc.calculate(deepcopy(records))
     arc = AssetRatio()
